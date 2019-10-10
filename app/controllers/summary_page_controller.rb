7bdb5437393480099f2ecab010bed43d4433ea09
tori-takashi <tori.toutoshi@gmail.com>
2019-10-10 18:15:44 +0800

    add summary page

---
+++ app/controllers/summary_page_controller.rb
@@ -0,0 +1,10 @@
+class SummaryPageController < ApplicationController
+    def index
+        location_array = Location.pluck(:location)
+        aggregated_data_array = []
+        location_array.each do |location|
+            aggregated_data_array.append(Summary.where(location: location, summary_type: "detailed").last)
+        end
+        @latest_data = aggregated_data_array
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_041348) do
+ActiveRecord::Schema.define(version: 2019_10_10_072816) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"

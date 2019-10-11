791b621c98511dfa35047d48e8fa25fa9e6ebd9b
tori-takashi <tori.toutoshi@gmail.com>
2019-10-12 03:23:15 +0800

    change name to detailed

--- app/controllers/summary_page_controller.rb
+++ app/controllers/summary_page_controller.rb
@@ -2,10 +2,10 @@ class SummaryPageController < ApplicationController
     def index
         location_array = Location.pluck(:location)
         # [FIX ME]resource consuming, too much SQL queries
-        aggregated_data_array = []
+        detailed_data_array = []
         location_array.each do |location|
-            aggregated_data_array.append(Summary.where(location: location, summary_type: "detailed").last)
+            detailed_data_array.append(Summary.where(location: location, summary_type: "detailed").last)
         end
-        @latest_data = aggregated_data_array
+        @latest_data = detailed_data_array
     end
 end
